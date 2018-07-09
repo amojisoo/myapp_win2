b2810cb120ba100183a28a1c86de224d4e6eff47
lee <amojisoo@gmail.com>
2018-07-10 01:43:18 +0900

    work_02 : detail modify : work01 Finish

--- info/tests.py
+++ info/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase , Client
 
 # Create your tests here.
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+
+    def test_index_access(self):
+        pass
+
+    def test_fail_access(self):
+
+        res = self.c.get('/')
+        # status code => 200 ok
+        # status code => 302 Redirect
+        # status code => 404 Not Found
+        self.assertEqual( 200 , res.status_code )
