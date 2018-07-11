a1fdb50edc47fea9afe13816bb7aeb5ab7ae9def
lee <amojisoo@gmail.com>
2018-07-11 17:16:22 +0900

    work_03 : color admin : one page thema add

--- blog/urls.py
+++ blog/urls.py
@@ -10,12 +10,27 @@ from blog import views
 from blog.views import BlogListView
 from blog.views import BlogDetailView
 
+
+
 urlpatterns = [
 
-    path(''   , BlogListView.as_view()    , name="list"),
+    path(''   , views.index    , name="index_blog"),
     path('<int:pk>', BlogDetailView.as_view(), name="detail"),
 
     path('summary/', views.summary, name="index_blog"),
+
+    path('summary/', views.summary, name="index_blog"),
+
+    path("about", views.about , name="about_blog"),
+    path("summary", views.summary ,name="summary_blog"),
+    path("contact", views.contact, name="contact_blog"),
+    path("post", views.post, name="post_blog"),
+    path("posts", views.posts, name="posts_blog"),
+
+    path("forum", views.forum, name="forum_blog"),
+    path("forumdetail", views.forum_detail, name="forum_blog"),
+
+    path("one", views.one, name="one_blog"),
 
     #url(r"^aa/list$"   , CLASS_VIEW.VIEW_LIST.as_view(),     name="aftereffects_list")
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
-
+from django.shortcuts import render
 
 
 from django.views.generic import ListView
@@ -23,3 +23,31 @@ def summary(request):
     return HttpResponse( "summary" )
 
 
+def index(request):
+    return render_to_response('ABS_ColorAdmin/BLOG_index.html', {'name': u'Alice'})
+
+
+
+def about(request):
+    return render_to_response('ABS_ColorAdmin/BLOG_about.html', {'name': u'Alice'})
+
+
+def contact(request):
+    return render_to_response('ABS_ColorAdmin/BLOG_contact.html', {'name': u'Alice'})
+
+def posts(request):
+    return render_to_response('ABS_ColorAdmin/BLOG_posts.html', {'name': u'Alice'})
+
+def post(request):
+    return render_to_response('ABS_ColorAdmin/BLOG_post.html', {'name': u'Alice'})
+
+def forum(request):
+    return render_to_response('ABS_ColorAdmin/FORUM_index.html', {'name': u'Alice'})
+
+def forum_detail(request):
+    return render_to_response('ABS_ColorAdmin/FORUM_detail.html', {'name': u'Alice'})
+
+
+
+def one(request):
+    return render(request , 'ABS_ColorAdmin/onepage.html')
