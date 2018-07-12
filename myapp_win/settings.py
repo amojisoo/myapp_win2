ea7df2ad7e748be38d1b50cba54ace2a72e3c66a
lee <amojisoo@gmail.com>
2018-07-12 15:49:01 +0900

    work_03 : color admin : end

--- myapp_win/settings.py
+++ myapp_win/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
